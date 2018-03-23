87ed5f6baf7b278a20eea30d5ef1095d7e1b6550
Showsay You <akizuki.c10.l65@gmail.com>
2018-03-23 11:21:03 +0900

    Define ControllerAbstract and ViewAbstract

---
+++ src/Controller/ControllerAbstract.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Bellisq\WebApplication\Controller;
+
+
+/**
+ * [Abstract Class] Controller
+ *
+ * @author Showsay You <akizuki.c10.l65@gmail.com>
+ * @copyright 2018 Bellisq. All Rights Reserved.
+ * @package bellisq/web-app
+ * @since 1.0.0
+ */
+abstract class ControllerAbstract
+{
+
+}
---
+++ src/View/ViewAbstract.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Bellisq\WebApplication\View;
+
+
+/**
+ * [Abstract Class] View
+ *
+ * @author Showsay You <akizuki.c10.l65@gmail.com>
+ * @copyright 2018 Bellisq. All Rights Reserved.
+ * @package bellisq/web-app
+ * @since 1.0.0
+ */
+abstract class ViewAbstract
+{
+    /**
+     * Dispatch HTTP headers and body.
+     *
+     * Careful: some of HTTP headers may be already sent in the controller.
+     */
+    abstract public function dispatch(): void;
+}
